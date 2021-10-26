60bab821fd2823634ca12619b07ce9034bd25593
Vyacheslav Gudimov <gudimov7@gmail.com>
2021-10-26 09:52:16 +0300

    created winner check method, in case there is a winner colorize text, and promt with Toast

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -43,9 +44,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void winner() {
-        if(buttons[0].getText().toString().equals("X"))
-            Toast.makeText(this,"Winner",Toast.LENGTH_SHORT).show();
+        /**
+         * Win combinations:
+         *
+         * 0 == 1 == 2  |   0 == 3 == 6 |   0 == 4 == 8
+         * 3 == 4 == 5  |   1 == 4 == 7 |   2 == 4 == 6
+         * 6 == 7 == 8  |   2 == 5 == 8
+         *
+         */
 
+        String [] btnStrings = new String [9];
+        for(int i = 0; i < btnStrings.length; i ++) {
+            btnStrings[i] = buttons[i].getText().toString();
+        }
+        if(btnStrings[0].equals(btnStrings[1]) && btnStrings[1].equals(btnStrings[2]) && !btnStrings[0].equals("")) {
+            colorWinner(0, 1, 2);
+            return;
+        }
+        else if(btnStrings[3].equals(btnStrings[4]) && btnStrings[4].equals(btnStrings[5]) && !btnStrings[3].equals("")) {
+            colorWinner(3, 4, 5);
+            return;
+        }
+        else if(btnStrings[6].equals(btnStrings[7]) && btnStrings[7].equals(btnStrings[8]) && !btnStrings[6].equals("")) {
+            colorWinner(6, 7, 8);
+            return;
+        }
+        else if(btnStrings[0].equals(btnStrings[3]) && btnStrings[3].equals(btnStrings[6]) && !btnStrings[0].equals("")) {
+            colorWinner(0, 3, 6);
+            return;
+        }
+        else if(btnStrings[1].equals(btnStrings[4]) && btnStrings[4].equals(btnStrings[7]) && !btnStrings[1].equals("")) {
+            colorWinner(1, 4, 7);
+            return;
+        }
+        else if(btnStrings[2].equals(btnStrings[5]) && btnStrings[5].equals(btnStrings[8]) && !btnStrings[2].equals("")) {
+            colorWinner(2, 5, 8);
+            return;
+        }
+        else if(btnStrings[0].equals(btnStrings[4]) && btnStrings[4].equals(btnStrings[8]) && !btnStrings[0].equals("")) {
+            colorWinner(0, 4, 8);
+            return;
+        }
+        else if(btnStrings[2].equals(btnStrings[4]) && btnStrings[4].equals(btnStrings[6]) && !btnStrings[2].equals("")) {
+            colorWinner(2, 4, 6);
+            return;
+        }
+
+    }
+    private void colorWinner(int btn1, int btn2, int btn3) {
+        buttons[btn1].setTextColor(Color.RED);
+        buttons[btn2].setTextColor(Color.RED);
+        buttons[btn3].setTextColor(Color.RED);
+        Toast.makeText(this, "And the Winner is :" + buttons[btn1].getText().toString(), Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -55,6 +105,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 b.setText(turnsCount % 2 == 0 ? "X": "O");
         }
         turnsCount ++;
+        winner();
 
     }
 }
